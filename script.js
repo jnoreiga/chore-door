b1d1d319e17b836daeeb0cb3348fc89e62253054
Anthony C <gitanthony@yahoo.com>
2022-01-19 09:57:18 -0500

    btn fully fxnal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // door imgs
-const doorClosed =
+const closedDoor =
     'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
 const robotDoor =
     'https://content.codecademy.com/projects/chore-door/images/robot.svg';
@@ -16,37 +16,26 @@ const door2 = document.getElementById('door2');
 const door3 = document.getElementById('door3');
 const btn = document.querySelector('button');
 
+const isBot = door => (door.src === robotDoor ? true : false);
+const isClicked = door => (door.src === closedDoor ? false : true);
+
 let ranDoor1;
 let ranDoor2;
 let ranDoor3;
-let numClosedDoors = 3;
+let currentlyPlaying;
+let numClosedDoors;
+let currentStreak = 0;
+let bestStreak = 0;
 
-const isBot = door => (door.src === doorClosed ? true : false);
+const gameBegin = () => {
+    numClosedDoors = 3;
+    currentlyPlaying = true;
+    btn.textContent = 'Good Luck!';
 
-// door event
-door1.addEventListener('click', () => {
-    if (door1.src === doorClosed) {
-        door1.src = ranDoor1;
-        playDoor();
-        // console.log(numClosedDoors);
-    }
-});
-door2.addEventListener('click', () => {
-    if (door2.src === doorClosed) {
-        door2.src = ranDoor2;
-        playDoor();
-        // console.log(numClosedDoors);
-    }
-});
-door3.addEventListener('click', () => {
-    if (door3.src === doorClosed) {
-        door3.src = ranDoor3;
-        playDoor();
-        // console.log(numClosedDoors);
-    }
-});
+    door1.src = closedDoor;
+    door2.src = closedDoor;
+    door3.src = closedDoor;
 
-const ranDoorGenerator = () => {
     const ranNum = Math.floor(Math.random() * 3);
     if (ranNum === 0) {
         ranDoor1 = robotDoor;
@@ -62,27 +51,56 @@ const ranDoorGenerator = () => {
         ranDoor3 = robotDoor;
     }
 };
-ranDoorGenerator();
+gameBegin();
+
+// door event
+door1.addEventListener('click', () => {
+    if (currentlyPlaying && !isClicked(door1)) {
+        door1.src = ranDoor1;
+        playDoor(door1);
+        // console.log(numClosedDoors);
+        // console.log(isBot(door1));
+    }
+});
+door2.addEventListener('click', () => {
+    if (currentlyPlaying && !isClicked(door2)) {
+        door2.src = ranDoor2;
+        playDoor(door2);
+        // console.log(numClosedDoors);
+    }
+});
+door3.addEventListener('click', () => {
+    if (currentlyPlaying && !isClicked(door3)) {
+        door3.src = ranDoor3;
+        playDoor(door3);
+        // console.log(numClosedDoors);
+    }
+});
 
 // win/loss
-const playDoor = () => {
+const playDoor = door => {
     numClosedDoors--;
 
     if (numClosedDoors === 0) {
+        gameOver('win');
+    } else if (isBot(door)) {
         gameOver();
     }
 };
 
-const gameOver = () => {};
+const gameOver = status => {
+    currentlyPlaying = false;
+
+    if (status === 'win') {
+        btn.textContent = 'You Win! Play Again!';
+    } else {
+        btn.textContent = 'Game over! Try Again!';
+    }
+};
 
 // restart game
 btn.addEventListener('click', () => {
-    door1.src = doorClosed;
-    door2.src = doorClosed;
-    door3.src = doorClosed;
-
-    numClosedDoors = 3;
-    ranDoorGenerator();
+    if (!currentlyPlaying) gameBegin();
 });
 
 // Counters
